9e40144ce1e1b8c18b9a7cac61eb11b9e1f3c6ee
Santiago Sánchez <SSANCHEZ@SSANCHEZ-2.local>
2018-10-26 11:38:14 -0500

    logo

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -6,7 +6,7 @@ import logo from '../../img/logoNegro.png';
 function Header(props){
     return(
     <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" height='109px' width='314px' />
+        <img src={logo} className="App-logo" alt='logo' height='109px' width='314px' />
         <h1 className="App-title"> Registro de Paradas</h1>
     </header>
     )
