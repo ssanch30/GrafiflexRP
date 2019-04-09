05c75c0f12d6ff522efa779b05f2869494e535af
Santiago Sánchez <SSANCHEZ@SSANCHEZ-2.local>
2019-04-09 14:50:40 -0500

    knex bug

--- server/db/migrations/20180912180707_create_stops_table.js
+++ server/db/migrations/20180912180707_create_stops_table.js
@@ -1,5 +1,5 @@
 exports.up = async function(knex) {
-      await  knex.schema.dropTableIfExists('stops').then(function (){createTable('stops',(table) => {
+      await  knex.schema.dropTableIfExists('stops').then(function (){knex.schema.createTable('stops',(table) => {
             table.increments('id').primary().unsigned()
             table.integer('user_id').notNullable().unsigned()
             table.integer('stoptype_id').notNullable().unsigned()
